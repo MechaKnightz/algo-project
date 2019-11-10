907aa57669abcff8d8831305f0c2c60421bad758
MechaKnightz <mechaknightz@hotmail.com>
2019-11-10 23:40:40 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: IndexComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
